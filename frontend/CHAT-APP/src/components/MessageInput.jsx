4179294ca2751f0ffc68f48e1b6e916137254b63
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2025-03-13 04:03:49 -0900

    time converter

--- frontend/CHAT-APP/src/components/MessageInput.jsx
+++ frontend/CHAT-APP/src/components/MessageInput.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
+import { useChatStore } from '../store/useChatStore'
 const MessageInput = () => {
 	const [text, stText] = useState('')
 	const [imagePreview, setImagePreview] = useState(null)
 	const fileInputRef = useRef(null)
+	const { sendMessage } = useChatStore()
+
+	const handleImageChange = (e) => {}
+	const removeImage = () => {}
+	const handleSendMessage = async (e) => {}
 	return <div>message input</div>
 }
 export default MessageInput
