a38f9e68ef41231e0556892a013537aa9ed25660
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2025-09-14 09:22:00 -0900

    real finalcommit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,16 +6,28 @@ import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 // import bodyParser from 'body-parser'
 import cors from 'cors'
+import path from 'path'
 import { app, server } from './lib/socket.js'
 dotenv.config()
 
-app.use(express.json({ limit: '500mb' }))
+const __dirname = path.resolve()
+
 app.use(cookieParser())
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
 app.use('/api/auth', authRouter)
 app.use('/api', messgaeRouter)
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join('../frontend/CHAT-APP/dist')))
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.join(__dirname, '../frontend/CHAT-APP', 'dist', 'index.html')
+		)
+	})
+}
+
 const Port = process.env.PORT
 server.listen(Port, () => {
 	console.log('server is running on port 5001')
